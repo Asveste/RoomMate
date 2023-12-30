a5e98b8ec19ca3a2469842fe7a976efbc2afde41
Elijah3791 <elijah.hirschfeld03@gmail.com>
2023-12-30 12:44:28 +0100

    Test Commit, Made to try Pull

--- src/main/java/roommate/Main.java
+++ src/main/java/roommate/Main.java
@@ -2,6 +2,6 @@ package roommate;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello World!");
+        System.out.print("Hello Middle-Earth!");
     }
 }
