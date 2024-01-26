a8e64fdc04e16b4ef76c703bc6b8f5d5764f10e1
Asveste <23436647+Asveste@users.noreply.github.com>
2024-01-26 16:43:48 +0100

    Add Mappings for subpages

--- src/main/java/roommate/adapter/web/RoomMateController.java
+++ src/main/java/roommate/adapter/web/RoomMateController.java
@@ -11,8 +11,23 @@ public class RoomMateController {
         return "index";
     }
 
+    @GetMapping("/workspace_booking")
+    public String workspaceBooking() {
+        return "workspace_booking";
+    }
+
     @GetMapping("/room_overview")
     public String roomOverview() {
         return "room_overview";
+    }
+
+    @GetMapping("/user_bookings")
+    public String userBookings() {
+        return "user_bookings";
+    }
+
+    @GetMapping("/rules_booking")
+    public String rulesBooking() {
+        return "rules_booking";
     }
 }
