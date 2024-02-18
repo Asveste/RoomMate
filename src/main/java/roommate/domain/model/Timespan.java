283f936600c758ad4e76a82254d68756e44cfd3f
Cecil <cecil.dunkel@gmail.com>
2024-02-18 16:30:41 +0100

    Attempt to fix spacing issue in Timespan

--- src/main/java/roommate/domain/model/Timespan.java
+++ src/main/java/roommate/domain/model/Timespan.java
@@ -1,10 +1,10 @@
 package roommate.domain.model;
 
 import jakarta.validation.constraints.FutureOrPresent;
-
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 public record Timespan(@FutureOrPresent LocalDate date, LocalTime startTime, LocalTime endTime,
                        Integer workspaceId) {
 }
+// For fixing the spacing
