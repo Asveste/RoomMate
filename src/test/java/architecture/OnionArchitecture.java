ef315274903c081d2b8a72f4bb647efadfa66807
Asveste <23436647+Asveste@users.noreply.github.com>
2024-02-18 16:16:59 +0100

    Add ArchUnit OnionArchitecture testing

---
+++ src/test/java/architecture/OnionArchitecture.java
@@ -0,0 +1,23 @@
+package architecture;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import roommate.RoomMateApplication;
+
+import static com.tngtech.archunit.library.Architectures.onionArchitecture;
+
+
+@AnalyzeClasses(packagesOf = RoomMateApplication.class)
+public class OnionArchitecture {
+
+    @ArchTest
+    ArchRule rule = onionArchitecture()
+            .domainModels("roommate.domain.model..")
+            .domainServices("roommate.applicationservice..")
+            .applicationServices("roommate.applicationservice..")
+            .adapter("web", "roommate.adapter.web")
+            .adapter("persistence", "roommate.adapter.db");
+
+
+}
