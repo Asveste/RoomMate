6f7fb72ab5892915750c6cefbb739d7114695f24
Cecil <cecil.dunkel@gmail.com>
2024-02-18 17:46:14 +0100

    Added DatabaseTest class

---
+++ src/test/java/roommate/adapter/db/DatabaseTest.java
@@ -0,0 +1,40 @@
+package roommate.adapter.db;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.test.context.ActiveProfiles;
+import roommate.applicationservice.WorkspaceRepository;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJdbcTest
+@ActiveProfiles("test")
+public class DatabaseTest {
+
+    @Autowired
+    WorkspaceDao dataRepository;
+
+        @BeforeEach
+        void setup() {
+
+        }
+
+        @Test
+        @DisplayName("Ein Workspace kann gespeichert und geladen werden")
+        void test_1() {
+            // Arrange
+            // Act
+            // Assert
+        }
+
+        @Test
+        @DisplayName("Wenn es mehrere Workspaces gibt, werden alle gefunden")
+        void test_2() {
+            //Arrange
+            // Act
+            // Assert
+        }
+
+}
