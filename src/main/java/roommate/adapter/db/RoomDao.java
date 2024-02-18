c5d3ea2dc376bf3964207bf2eadbb64f04d0e2b9
Elijah3791 <elijah.hirschfeld03@gmail.com>
2024-02-18 18:30:34 +0100

    Add RoomDao.java

---
+++ src/main/java/roommate/adapter/db/RoomDao.java
@@ -0,0 +1,9 @@
+package roommate.adapter.db;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface RoomDao extends CrudRepository<Room, Integer> {
+    Optional<Room> findById(Integer id);
+}
