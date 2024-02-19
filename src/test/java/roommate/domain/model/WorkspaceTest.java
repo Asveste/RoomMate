7bcb7a8fba3f2b28885a7dd929345c775eb6d9ac
Asveste <23436647+Asveste@users.noreply.github.com>
2024-02-19 04:46:45 +0100

    Refactor and fix WorkspaceTest.java

--- src/test/java/roommate/domain/model/WorkspaceTest.java
+++ src/test/java/roommate/domain/model/WorkspaceTest.java
@@ -5,20 +5,21 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.Set;
+import java.util.List;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.mock;
 
 class WorkspaceTest {
     @Test
     @DisplayName("Wenn bei Timespan die startTime nach der endTime ist, wird false zurückgegeben")
     void isValid() {
-        Room room = mock(Room.class);
-        Workspace workspace = new Workspace(0, room, Set.of(), Set.of(new Timespan(LocalDate.now(), LocalTime.of(16, 0), LocalTime.of(15, 59), 0)));
-        Set<Timespan> existingReservations = workspace.getExistingReservations();
-        Timespan timespan = existingReservations.stream().findFirst().orElse(new Timespan(LocalDate.now(), LocalTime.of(16, 0), LocalTime.of(15, 0), 0));
+        Workspace workspace = new Workspace(0, UUID.randomUUID());
+        workspace.addReservation(new Timespan(LocalDate.now(), LocalTime.of(16, 0), LocalTime.of(15, 59)));
+
+        List<Timespan> existingReservations = workspace.existingReservations();
+        Timespan timespan = existingReservations.stream().findFirst().orElse(new Timespan(LocalDate.now(), LocalTime.of(10, 0), LocalTime.of(12, 0)));
 
         boolean valid = workspace.isValid(timespan);
 
@@ -28,10 +29,11 @@ class WorkspaceTest {
     @Test
     @DisplayName("Wenn bei Timespan die endTime == startTime ist, wird false zurückgegeben")
     void isValid2() {
-        Room room = mock(Room.class);
-        Workspace workspace = new Workspace(1, room, Set.of(), Set.of(new Timespan(LocalDate.now(), LocalTime.of(14, 0), LocalTime.of(14, 0), 1)));
-        Set<Timespan> existingReservations = workspace.getExistingReservations();
-        Timespan timespan = existingReservations.stream().findFirst().orElse(new Timespan(LocalDate.now(), LocalTime.of(14, 0), LocalTime.of(14, 0), 1));
+        Workspace workspace = new Workspace(0, UUID.randomUUID());
+        workspace.addReservation(new Timespan(LocalDate.now(), LocalTime.of(14, 0), LocalTime.of(14, 0)));
+
+        List<Timespan> existingReservations = workspace.existingReservations();
+        Timespan timespan = existingReservations.stream().findFirst().orElse(new Timespan(LocalDate.now(), LocalTime.of(10, 0), LocalTime.of(12, 0)));
 
         boolean valid = workspace.isValid(timespan);
 
@@ -39,35 +41,35 @@ class WorkspaceTest {
     }
 
     @Test
-    @DisplayName("Wenn sich zwei Zeiten überlappen wird true zurückgegeben")
+    @DisplayName("Wenn sich Zeiten überlappen wird true zurückgegeben")
     void isOverlap() {
-        Room room = mock(Room.class);
-        Workspace workspace = new Workspace(2, room, Set.of(), Set.of(new Timespan(LocalDate.now().plusDays(1), LocalTime.of(14, 0), LocalTime.of(16, 0), 2)));
+        Workspace workspace = new Workspace(0, UUID.randomUUID());
+        workspace.addReservation(new Timespan(LocalDate.now(), LocalTime.of(14, 0), LocalTime.of(16, 0)));
 
-        boolean overlap = workspace.isOverlap(new Timespan(LocalDate.now().plusDays(1), LocalTime.of(15, 0), LocalTime.of(17, 0), 2));
+        boolean valid = workspace.isOverlap(new Timespan(LocalDate.now(), LocalTime.of(15, 0), LocalTime.of(17, 0)));
 
-        assertTrue(overlap);
+        assertTrue(valid);
     }
 
     @Test
-    @DisplayName("Wenn sich zwei Zeiten nicht überlappen wird false zurückgegeben")
+    @DisplayName("Wenn sich Zeiten nicht überlappen wird false zurückgegeben")
     void isOverlap2() {
-        Room room = mock(Room.class);
-        Workspace workspace = new Workspace(3, room, Set.of(), Set.of(new Timespan(LocalDate.now().plusDays(1), LocalTime.of(14, 0), LocalTime.of(15, 0), 3)));
+        Workspace workspace = new Workspace(0, UUID.randomUUID());
+        workspace.addReservation(new Timespan(LocalDate.now(), LocalTime.of(14, 0), LocalTime.of(16, 0)));
 
-        boolean overlap = workspace.isOverlap(new Timespan(LocalDate.now().plusDays(1), LocalTime.of(15, 0), LocalTime.of(17, 0), 3));
+        boolean valid = workspace.isOverlap(new Timespan(LocalDate.now(), LocalTime.of(16, 1), LocalTime.of(17, 0)));
 
-        assertFalse(overlap);
+        assertFalse(valid);
     }
 
     @Test
-    @DisplayName("Wenn zwei Zeiten gleich sind aber das Datum unterschiedlich wird false zurückgegeben")
+    @DisplayName("Wenn Zeiten gleich sind aber das Datum unterschiedlich wird false zurückgegeben")
     void isOverlap3() {
-        Room room = mock(Room.class);
-        Workspace workspace = new Workspace(4, room, Set.of(), Set.of(new Timespan(LocalDate.now().plusDays(1), LocalTime.of(14, 0), LocalTime.of(15, 0), 4)));
+        Workspace workspace = new Workspace(0, UUID.randomUUID());
+        workspace.addReservation(new Timespan(LocalDate.now(), LocalTime.of(14, 0), LocalTime.of(16, 0)));
 
-        boolean overlap = workspace.isOverlap(new Timespan(LocalDate.now().plusDays(2), LocalTime.of(14, 0), LocalTime.of(15, 0), 4));
+        boolean valid = workspace.isOverlap(new Timespan(LocalDate.now().plusDays(1), LocalTime.of(14, 0), LocalTime.of(16, 0)));
 
-        assertFalse(overlap);
+        assertFalse(valid);
     }
 }
