c71b2144f14f8b130bdeda7ddb7965d065680cb7
Asveste <23436647+Asveste@users.noreply.github.com>
2024-02-19 04:47:20 +0100

    Partly implement BookingService.java

--- src/main/java/roommate/applicationservice/BookingService.java
+++ src/main/java/roommate/applicationservice/BookingService.java
@@ -1,9 +1,30 @@
 package roommate.applicationservice;
 
+import roommate.domain.model.Trait;
+import roommate.domain.model.Workspace;
+
+import java.util.Collection;
+import java.util.List;
+
 public class BookingService {
     private final WorkspaceRepository repo;
 
     public BookingService(WorkspaceRepository repo) {
         this.repo = repo;
+    }
+
+    public List<Workspace> allWorkspaces() {
+        return repo.findAll().stream().sorted().toList();
+    }
+
+    public Workspace workspace(Integer id) {
+        return repo.findById(id)
+                .orElseThrow(NotExistentException::new);
+    }
+
+    public List<Workspace> allWorkspacesByTraits(Collection<Trait> traits) {
+        return repo.findAll().stream()
+                .filter(workspace -> workspace.traits().containsAll(traits))
+                .toList();
     }
 }
