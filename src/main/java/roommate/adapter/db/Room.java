7891629128fa6e1fb8a7daedcc676953c4f498d6
Asveste <23436647+Asveste@users.noreply.github.com>
2024-02-19 04:44:29 +0100

    Remove Room and make part of Workspace field attribute
    Only commented out, to leave open a possible change back

--- src/main/java/roommate/adapter/db/Room.java
+++ src/main/java/roommate/adapter/db/Room.java
@@ -1,8 +1,8 @@
-package roommate.adapter.db;
-
-import org.springframework.data.annotation.Id;
-
-import java.util.UUID;
-
-public record Room(UUID uuid, @Id Integer id, String name) {
-}
+//package roommate.adapter.db;
+//
+//import org.springframework.data.annotation.Id;
+//
+//import java.util.UUID;
+//
+//public record Room(UUID uuid, @Id Integer id, String name) {
+//}
--- src/main/java/roommate/domain/model/Room.java
+++ src/main/java/roommate/domain/model/Room.java
@@ -1,42 +1,42 @@
-package roommate.domain.model;
-
-import java.util.UUID;
-
-public class Room {
-    private final UUID uuid;
-    private final Integer id;
-    private final String name;
-
-    public Room(String name) {
-        this.uuid = UUID.randomUUID();
-        this.id = null;
-        this.name = name;
-    }
-
-    public Room(UUID uuid, Integer id, String name) {
-        this.uuid = uuid;
-        this.id = id;
-        this.name = name;
-    }
-
-    public UUID getUuid() {
-        return uuid;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public String toString() {
-        return "Room{" +
-                "uuid=" + uuid +
-                ", id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
-}
+//package roommate.domain.model;
+//
+//import java.util.UUID;
+//
+//public class Room {
+//    private final UUID uuid;
+//    private final Integer roomId;
+//    private final String name;
+//
+//    public Room(String name) {
+//        this.uuid = UUID.randomUUID();
+//        this.roomId = null;
+//        this.name = name;
+//    }
+//
+//    public Room(UUID uuid, Integer id, String name) {
+//        this.uuid = uuid;
+//        this.roomId = id;
+//        this.name = name;
+//    }
+//
+//    public UUID uuid() {
+//        return uuid;
+//    }
+//
+//    public Integer roomId() {
+//        return roomId;
+//    }
+//
+//    public String name() {
+//        return name;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Room{" +
+//                "uuid=" + uuid +
+//                ", id=" + roomId +
+//                ", name='" + name + '\'' +
+//                '}';
+//    }
+//}
